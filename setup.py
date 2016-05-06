f295d10312c67f71cee1eafc3eef069384bc045f
Robert Cope <rpcope1@gmail.com>
2016-05-05 19:38:29 -0600

    Bad indent in setup long description.

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from textwrap import dedent
 from ductworks import __version__, __author__, __author_email__
 
 setup(
@@ -8,7 +9,7 @@ setup(
     url="https://bitbucket.org/rpcope1/ductworks",
     version=__version__,
     description="A simple IPC library for less simple situations.",
-    long_description="""
+    long_description=dedent("""
     Ductworks is an IPC library for Python, similar to the Pipe object from the multiprocessing library. Like Pipe,
     ductworks exposes a MessageDuct API with very simple send and receive semantics for end users (no worrying about
     streams, each send call corresponds to one recv call), while easily allowing for more flexible usage like having
@@ -17,7 +18,7 @@ setup(
     JSON by default) and uses regular BSD sockets; this makes connecting up programs in different languages (or even on
     different systems) relatively straightforward. Ductworks aims to make it simple to get socket pairs, when inheriting
     a anonymous socket pair is difficult or outright infeasible, and to make IPC easier and less stressful.
-    """,
+    """),
     packages=['ductworks'],
     install_requires=[],
     tests_require=['assertpy'],
